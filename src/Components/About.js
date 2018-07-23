16cf80b34dabff29ecab804eb1ea62a5e3014d60
Gabriel Anhaia <silvag@dbserver.com.br>
2018-07-23 19:03:35 -0300

    FEA: Adiciona estilo no blockquote.

--- src/Components/About.js
+++ src/Components/About.js
@@ -15,18 +15,25 @@ class About extends Component {
         color: '#767676',
         marginBottom: '2.3125rem',
         borderLeft: '2px solid',
-        zIndex: -1
+        zIndex: -999
     }
 
     render() {
         return (
-            <div className="container">
+            <div className="container" style={{zIndex: -999}}>
                 <div className='row'>
-                    <div style={{zIndex: -1}} className='pt-4 col d-flex justify-content-center'>
+                    <div style={{zIndex: -999}} className='pt-4 col d-flex justify-content-center'>
                         <img className="" src="/images/art/drawing_house2.png" alt="First slide"/>
                     </div>
-                    <div className='pt-4 col d-flex justify-content-center align-self-center'>
-                        <blockquote style={this.style}>Nullam at tellus a ante dictum sagittis. Aenean malesuada, turpis non aliquam blandit, nisl dui pellentesque tortor, malesuada consectetur sem lectus sed lacus. Nulla nec turpis mattis dui ornare blandit. Ut leo nisl, tempus ut bibendum in, iaculis quis felis. Aliquam et lorem vel dolor tincidunt vulputate vel sed lacus. Morbi tristique elementum vehicula. Duis sem tellus, porta in leo sed, porttitor aliquet magna. Ut cursus erat sed pulvinar semper. Donec gravida porttitor arcu, sed vulputate libero. Morbi non justo ac tellus tempus ornare. Nam tortor augue, commodo eget lobortis non, consectetur eget eros. In hac habitasse platea dictumst. Nam congue odio neque, in tempus sapien faucibus non.&nbsp;</blockquote>
+                    <div className='pt-4 col d-flex justify-content-center align-self-center' style={{zIndex: -999}}>
+                        <blockquote className='pl-5' style={{
+                            borderLeft: '5px solid #eee'
+                        }}>
+                            <p>A fanatic is one who can't change his mind and won't change the subject.A fanatic is one who can't change his mind and won't change the subject.A fanatic is one who can't change his mind and won't change the subject.</p>
+                            <p>A fanatic is one who can't change his mind and won't change the subject.A fanatic is one who can't change his mind and won't change the subject.A fanatic is one who can't change his mind and won't change the subject.</p>
+                            <p>A fanatic is one who can't change his mind and won't change the subject.A fanatic is one who can't change his mind and won't change the subject.A fanatic is one who can't change his mind and won't change the subject.</p>
+                            <small>by <cite>Winston Churchill</cite></small>
+                        </blockquote>
                     </div>
                 </div>
             </div>
